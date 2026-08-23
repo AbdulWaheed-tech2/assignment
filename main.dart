a733735c30119486b59146b0839ba2eca8bac901
Abdulwaheed-tech2 <abdulwaheed3248@gmail.com>
2026-08-24 00:17:08 +0500

    first comit

---
+++ main.dart
@@ -0,0 +1,19 @@
+void main() {
+  // List numbers = [1, 28, 4, 23, 15];
+  // print(numbers);
+  // numbers.add(10);
+  // print(numbers);
+  // numbers.addAll([6, 8, 9]);
+  // print(numbers);
+  // numbers.remove(4);
+  // numbers.removeAt(3);
+  // print(numbers);
+  // print(numbers.length);
+
+  List values = [12, 43, 56423, 21, 24, 31, 333, 42, 46, 1, 0, 112, 12];
+
+  values.sort();
+  print(values);
+  values = values.reversed.toList();
+  print(values);
+}
