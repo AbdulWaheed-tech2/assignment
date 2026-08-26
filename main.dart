d3ec8ba452ea7596fc2b6a20d2a342b13a461f5e
Abdulwaheed-tech2 <abdulwaheed3248@gmail.com>
2026-08-26 23:30:37 +0500

    updated main.dart

--- main.dart
+++ main.dart
@@ -10,10 +10,94 @@ void main() {
   // print(numbers);
   // print(numbers.length);
 
-  List values = [12, 43, 56423, 21, 24, 31, 333, 42, 46, 1, 0, 112, 12];
+  // List values = [12, 43, 56423, 21, 24, 31, 333, 42, 46, 1, 0, 112, 12];
 
-  values.sort();
-  print(values);
-  values = values.reversed.toList();
-  print(values);
+  // values.sort();
+  // print(values);
+  // values = values.reversed.toList();
+  // print(values);
+
+  // int length = 5;
+  // int breadth = 5;
+  // if (length == breadth) {
+  //   print("Square");
+  // } else {
+  //   print("Rectangle");
+  // }
+
+  // int length = 5;
+  // int breadth = 8;
+  // if (length == breadth) {
+  //   print("Square");
+  // } else {
+  //   print("Rectangle");
+  // }
+
+  // int age1 = 38;
+  // int age2 = 48;
+  // if (age1 >= age2) {
+  //   print("age1 is oldest");
+  // } else {
+  //   print("age2 is oldest");
+  // }
+
+  // int classesHeld = 16;
+  // int classesAttended = 10;
+  // double percentage = (classesAttended / classesHeld) * 100;
+  // if (percentage < 75) {
+  //   print("not allowed in exam");
+  // } else {
+  //   print("allowed in exam");
+  // }
+
+  // int year = 2024;
+  // if (year % 4 == 0) {
+  //   print("leap year");
+  // } else {
+  //   print("not leap year");
+  // }
+
+  // int temperature = 42;
+  // if (temperature < 0) {
+  //   print("freezing weather");
+  // } else if (temperature < 10) {
+  //   print("Very cold weather");
+  // } else if (temperature < 20) {
+  //   print("Cold weather");
+  // } else if (temperature < 30) {
+  //   print("Normal temp");
+  // } else if (temperature < 40) {
+  //   print("Its hot");
+  // } else {
+  //   print("Its very hot");
+  // }
+
+  // String alphabet = "e";
+  // if (alphabet == "a" ||
+  //     alphabet == "e" ||
+  //     alphabet == "i" ||
+  //     alphabet == "o" ||
+  //     alphabet == "u") {
+  //   print("Vowel");
+  // } else {
+  //   print("Consonant");
+  // }
+
+  int customerId = 1001;
+  String name = "james";
+  int unitsconsumed = 800;
+  double rate = 0;
+
+  if (unitsconsumed <= 199) {
+    rate = 1.20;
+  } else if (unitsconsumed < 400) {
+    rate = 1.50;
+  } else if (unitsconsumed < 600) {
+    rate = 1.80;
+  } else {
+    rate = 2.00;
+  }
+
+  double billamount = unitsconsumed * rate;
+  print("Net Bill Amount : $billamount");
 }
